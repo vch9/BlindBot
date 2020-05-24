f2d5f187895305a4e84f397da3fdb4f0999b2678
vch9 <valentinchb@gmail.com>
2020-05-24 12:36:35 +0200

    Login script

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.on('ready', () => {
+    console.log(`Logged in as ${client.user.tag}!`);
+});
+
+/* 
+    The private token must be in a file "./token.js"
+    token.js ->
+        const token = 'your_token';
+        exports.token = token;
+*/
+const Token = require('./token.js');
+client.login(Token.token);
