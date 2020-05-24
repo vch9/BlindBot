84e149405a013aa780c1a3a872441e1a11011e32
vch9 <valentinchb@gmail.com>
2020-05-24 14:49:01 +0200

    Join voice channel

--- src/cmd.js
+++ src/cmd.js
@@ -1,9 +1,12 @@
 const Discord = require('discord.js');
 const Common = require('./common.js');
+const Music = require('./music.js');
 
-function exec (client, msg) {
+function exec (msg) {
     if (msg.content.startsWith("?help")) {
         Common.error_msg(msg);
+    } else if (msg.content.startsWith("?play")) {
+        Music.play(msg);
     } else {
         msg.reply('Try `?help` for help');
     }
@@ -14,7 +17,7 @@ exports.listen = function (self, client) {
         if (msg.author.username != self &&
             msg.channel.name === "dev" &&
             msg.content[0] === "?") {
-            exec(client, msg);
+            exec(msg);
         }
     });
 }
---
+++ src/music.js
@@ -0,0 +1,38 @@
+const Common = require('./common.js');
+
+const queue = new Map();
+const servQueue = false;
+
+async function connect (queue, msg, voiceChannel) {
+    try {
+        let conn = await voiceChannel.join();
+    } catch (err){
+        console.log(err);
+        queue.delete(message.guild.id);
+        msg.reply('I could not join your voice channel!');
+    }
+}
+
+exports.play = async function (msg) {
+    let args = msg.content.substr(6);
+    const voiceChannel = msg.member.voice.channel;
+    const textChannel = msg.channel;
+
+    if (!voiceChannel) {
+        msg.reply("You need to be in a voice channel");
+        return;
+    }
+
+    if (!servQueue) {
+        let newQueue = {
+            active: true,
+            songs: [],
+            voiceChannel: voiceChannel,
+            textChannel: textChannel
+        };
+        queue.set(msg.guild.id, newQueue);
+        connect(queue, msg, voiceChannel);
+    } else {
+        servQueue.song.push(args);
+    }
+}
