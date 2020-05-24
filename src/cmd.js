fa212bfe434b53260ae05a693909657fd3dbe266
vch9 <valentinchb@gmail.com>
2020-05-24 13:59:45 +0200

    Parse user commands

--- src/cmd.js
+++ src/cmd.js
@@ -3,12 +3,26 @@ function error_msg (msg) {
     msg.reply(not_impl);
 }
 
+function exec (client, msg) {
+    let cmd = msg.content.substr(1);
+    cmd.trim();
+    let cmds = cmd.split(' ');
+    switch (cmds[0]) {
+        case 'help':
+            error_msg(msg);
+            break;
+        default:
+            msg.reply('Try `?help` for help');
+            break;
+    }
+}
+
 exports.listen = function (self, client) {
     client.on('message', msg => {
         if (msg.author.username != self &&
             msg.channel.name === "dev" &&
             msg.content[0] === "?") {
-            error_msg(msg);
+            exec(client, msg);
         }
     });
 }
