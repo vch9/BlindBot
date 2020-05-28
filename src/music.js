7942a26da09359dd1f96bcde48a58656bb769849
vch9 <valentinchb@gmail.com>
2020-05-28 19:39:11 +0200

    Change song format to ytdl

--- src/music.js
+++ src/music.js
@@ -1,10 +1,12 @@
 const Common = require('./common.js');
+const ytdl = require('ytdl-core');
 
 const queue = new Map();
 
 async function connect (queue, msg, voiceChannel) {
     try {
         let conn = await voiceChannel.join();
+        return conn;
     } catch (err){
         console.log(err);
         queue.delete(message.guild.id);
@@ -13,9 +15,15 @@ async function connect (queue, msg, voiceChannel) {
 }
 
 exports.play = async function (msg) {
-    let args = msg.content.substr(6);
     const voiceChannel = msg.member.voice.channel;
     const textChannel = msg.channel;
+
+    const args = msg.content.substr(6);
+    const songInfo = await ytdl.getInfo(args);
+    const song = {
+        title: songInfo.title,
+        url: songInfo.video_url
+    };
 
     if (!voiceChannel) {
         msg.reply("You need to be in a voice channel");
@@ -28,11 +36,17 @@ exports.play = async function (msg) {
             active: true,
             songs: [],
             voiceChannel: voiceChannel,
-            textChannel: textChannel
+            textChannel: textChannel,
+            conn: null
         };
+        newQueue.songs.push(args);
+        newQueue.conn = connect(queue, msg, voiceChannel);
+
         queue.set(msg.guild.id, newQueue);
-        connect(queue, msg, voiceChannel);
+
+        playMusic(msg.guild.id);
     } else {
-        servQueue.songs.push(args);
+        servQueue.songs.push(song);
+        msg.channel.send(`${song.title} has been added to the queue.`);
     }
 }
