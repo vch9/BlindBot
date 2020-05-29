25c88166ab3bdd72fdf6f1f43e5a67ca4e0fb099
vch9 <valentinchb@gmail.com>
2020-05-30 00:59:35 +0200

    Display themes when game start

--- src/blindtest.js
+++ src/blindtest.js
@@ -1,7 +1,29 @@
 const common = require('./common.js');
+const Discord = require('discord.js');
+const fs = require('fs');
 
+const themes_path = './musics/';
 const games = new Map();
 
+function getThemes() {
+    return fs.readdirSync(themes_path);
+}
+
+function displayThemes (channel) {
+    let themes = getThemes();
+    let themes_str = '';
+    themes.forEach(theme => {
+        theme = theme.substr(0, theme.length-3);
+        themes_str += `* ${theme}\n`;
+    });
+
+    const msg = new Discord.MessageEmbed()
+        .setColor('#0099ff')
+        .setTitle('Starting the blind test party!')
+        .setDescription('Pick your theme:\n' + themes_str + '\nuse ?pick [theme]');
+    
+    channel.send(msg);
+}
 
 exports.start = function (msg) {
     if (!common.checkVoiceChannel(msg)) {
@@ -9,10 +31,13 @@ exports.start = function (msg) {
     }
 
     let game = {
-        active: true
+        active: true,
+        theme: null
     };
 
     games.set(msg.guild.id, game);
+
+    displayThemes(msg.channel);
 }
 
 exports.inGame = function (msg) {
