bfa76febc13c8eb60e336993a3aecdcf39e34c37
vch9 <valentinchb@gmail.com>
2020-05-29 22:24:16 +0200

    Export checkVoiceChannel to common

--- src/common.js
+++ src/common.js
@@ -4,3 +4,11 @@ exports.error_msg = function (msg) {
     const not_impl = "NOT_IMPLEMENTED";
     msg.reply(not_impl);
 }
+
+exports.checkVoiceChannel = function (msg) {
+    if (!msg.member.voice.channel) {
+        msg.reply('You have to be in a voice channel to execute that command!');
+        return false;
+    }
+    return true;
+}
