53879c4da9e76d262ab721851fb599ccee97d174
vch9 <valentinchb@gmail.com>
2020-06-07 20:17:04 +0200

    Add some movies songs

--- musics/movies.js
+++ musics/movies.js
@@ -1,3 +1,30 @@
 exports.list = [
-    ['Harry Potter', 'https://www.youtube.com/watch?v=Htaj3o3JD8I']
-]
+    ['Transformers', 'https://www.youtube.com/watch?v=3bTMjXRlaSQ'],
+    ['Back to the future', 'https://www.youtube.com/watch?v=3bTMjXRlaSQ'],
+    ['Mission impossible', 'https://www.youtube.com/watch?v=Agmu6WFdcLY'],
+    ['Harry Potter', 'https://www.youtube.com/watch?v=Htaj3o3JD8I'],
+    ['Star Wars', 'https://www.youtube.com/watch?v=_D0ZQPqeJkk'],
+    ['Cendrillon', 'https://www.youtube.com/watch?v=qbdgp989fRg'],
+    ['James Bond', 'https://www.youtube.com/watch?v=U9FzgsF2T-s'],
+    ['Ready Player One', 'https://www.youtube.com/watch?v=J93fc--VsaI'],
+    ['Lion King', 'https://www.youtube.com/watch?v=TGDXTZFleHQ'],
+    ['Gladiator', 'https://www.youtube.com/watch?v=8mYsSOZYgDE'],
+    ['The mazer runner', 'https://www.youtube.com/watch?v=dI25KCY9-Gg'],
+    ['Inception', 'https://www.youtube.com/watch?v=miymVzEpjR8'],
+    ['E.T', 'https://www.youtube.com/watch?v=O15x-B8PgeE'],
+    ['Grease', 'https://www.youtube.com/watch?v=vvTRUFYQY4M'],
+    ['Avatar', 'https://www.youtube.com/watch?v=i1BwcCuEOtM'],
+    ['Iron Man', 'https://www.youtube.com/watch?v=s5Cf2J64Xmk'],
+    ['The Dark Knight Rises', 'https://www.youtube.com/watch?v=iGx5a1ifSDs'],
+    ['Halloween', 'https://www.youtube.com/watch?v=lNMIlubmwOQ'],
+    ['Interstellar', 'https://www.youtube.com/watch?v=UDVtMYqUAyw'],
+    ['Ghost busters', 'https://www.youtube.com/watch?v=m9We2XsVZfc'],
+    ['The lord of the rings', 'https://www.youtube.com/watch?v=J93fc--VsaI'],
+    ['Titanic', 'https://www.youtube.com/watch?v=F2RnxZnubCM'],
+    ['Matrix', 'https://www.youtube.com/watch?v=pFS4zYWxzNA'],
+    ['Jurassic Park', 'https://www.youtube.com/watch?v=D8zlUUrFK-M'],
+    ['Indiana Jones', 'https://www.youtube.com/watch?v=-bTpp8PQSog'],
+    ['Jaws', 'https://www.youtube.com/watch?v=A9QTSyLwd4w'],
+    ['Pacific Rim', 'https://www.youtube.com/watch?v=1vU7XqToZso'],
+    ['Tarzan', 'https://www.youtube.com/watch?v=-WcHPFUwd6U']
+];
